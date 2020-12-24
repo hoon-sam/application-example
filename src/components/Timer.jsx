1eb5ac7b895bd9c4e862480129221b3ff958ee84
hypernova1 <hypemova@gmail.com>
2020-12-24 14:16:26 +0900

    feature: 타이머 구현

---
+++ src/components/Timer.jsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+export default function Timer() {
+
+  const [time, setTime] = useState(0);
+  const [isPause, setPause] = useState(false);
+  const countRef = useRef(null);
+
+  let countdown;
+
+  useEffect(() => {
+  }, [time]);
+
+  const start = () => {
+    if (isPause) return;
+    setPause(true);
+    countRef.current = setInterval(() => {
+      setTime((time) => time + 1);
+    }, 1000);
+  }
+
+  const pause = () => {
+    if (!isPause) return;
+    setPause(false);
+    clearInterval(countRef.current);
+  }
+
+  const stop = () => {
+    pause();
+    setTime(0);
+  }
+
+  const formatTime = () => {
+    const getSeconds = `0${(time % 60)}`.slice(-2);
+    const minutes = `${Math.floor(time / 60)}`;
+    const getMinutes = `0${minutes % 60}`.slice(-2);
+    const getHours = `0${Math.floor(time / 3600)}`.slice(-2);
+
+    return `${getHours} : ${getMinutes} : ${getSeconds}`;
+  }
+  
+
+  return (
+    <div>
+      <div>{ formatTime() }</div>
+      <button onClick={start}>시작</button>
+      <button onClick={pause}>일시정지</button>
+      <button onClick={stop}>중지</button>
+    </div>
+  );
+}
--- src/pages/Record.jsx
+++ src/pages/Record.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Timer from '../components/Timer';
 
 export default function Record() {
   return (
     <section>
       기록
+      <Timer />
     </section>
   )
 }
