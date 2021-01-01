4dc8f5128547a3efafdf8bcd3ae2c422dd2b215b
hypernova1 <chtlstjd01@gmail.com>
2021-01-01 14:17:31 +0900

    feature: 타이머 시간 주기적 요청 기능

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -4,28 +4,52 @@ export default function Timer() {
 
   const [time, setTime] = useState(0);
   const [isPause, setPause] = useState(false);
+  const [intervalId, setIntervalId] = useState(null);
   const countRef = useRef(null);
 
   useEffect(() => {
   }, [time]);
 
-  const start = () => {
+  const start = async () => {
     if (isPause) return;
     setPause(true);
-    countRef.current = setInterval(() => {
+    countRef.current = await setInterval(() => {
       setTime((time) => time + 1);
     }, 1000);
+    requestTime();
   }
 
   const pause = () => {
     if (!isPause) return;
     setPause(false);
+    clearInterval(intervalId);
     clearInterval(countRef.current);
   }
 
   const stop = () => {
     pause();
     setTime(0);
+    clearInterval(intervalId);
+  }
+
+  const requestTime = () => {
+    if (intervalId) {
+      clearInterval(intervalId);
+    }
+    setIntervalId(setInterval(() => {
+      console.log(time);
+      fetch('/api/timer', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: 'test@test.com',
+          time
+        })
+      }).then((res) => {
+      });
+    }, 1000));
   }
 
   const formatTime = () => {
